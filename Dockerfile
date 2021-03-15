9025039762e01e36819ca4b9d4043b230356f91b
nerdicwalker <nerdicwalker@gmail.com>
2021-03-15 16:34:13 +0100

    Ubuntu 18.04

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+#FROM ubuntu:latest
+FROM ubuntu:18.04
+RUN apt-get update
+RUN apt-get upgrade -y
+RUN apt-get install curl -y
+RUN apt-get install gnupg -y
+#RUN apt-get install libicu-dev -y
+RUN apt-get install python3-pip -y
+RUN apt-get install wget -y
+RUN apt-get install apt-transport-https -y
+RUN wget -qO - https://packages.irods.org/irods-signing-key.asc | apt-key add -
+RUN sh -c 'echo "deb [arch=amd64]  https://packages.irods.org/apt/ xenial main" | tee /etc/apt/sources.list.d/renci-irods.list'
+RUN apt-get update
+RUN apt-get -y install aptitude irods-runtime=4.2.6 irods-icommands=4.2.6
+RUN aptitude hold irods-runtime irods-icommands
+WORKDIR /root
+RUN mkdir /root/.irods
+COPY ./irods_environment.json /root/.irods/irods_environment.json
---
+++ deploy.sh
@@ -0,0 +1,42 @@
+#/bin/bash
+
+# read the .env file
+source .env
+
+# check if required environment variables are available
+iRODSHost=${irods_host?"Please declare environment variable: 'irods_host'."}
+iRODSPort=${irods_port?"Please declare environment variable: 'irods_port'."}
+iRODSHome=${irods_home?"Please declare environment variable: 'irods_home'."}
+iRODSUserName=${irods_user_name?"Please declare environment variable: 'irods_user_name'."}
+iRODSZoneName=${irods_zone_name?"Please declare environment variable: 'irods_zone_name'."}
+iRODSAuthenticationScheme=${irods_authentication_scheme?"Please declare environment variable: 'irods_authentication_scheme'."}
+iRODSEncryptionAlgorithm=${irods_encryption_algorithm?"Please declare environment variable: 'irods_encryption_algorithm'."}
+iRODSEncryptionKeySize=${irods_encryption_key_size?"Please declare environment variable: 'irods_encryption_key_size'."}
+iRODSEncryptionNumHashRounds=${irods_encryption_num_hash_rounds?"Please declare environment variable: 'irods_encryption_num_hash_rounds'."}
+iRODSEncryptionSaltSize=${irods_encryption_salt_size?"Please declare environment variable: 'irods_encryption_salt_size'."}
+iRODSClientServerNegotiation=${irods_client_server_negotiation?"Please declare environment variable: 'irods_client_server_negotiation'."}
+
+# creating irods_environment.json file
+cat << EOF > irods_environment.json
+{
+    "irods_host": "$iRODSHost",
+    "irods_port": $iRODSPort,
+    "irods_home": "$iRODSHome",
+    "irods_user_name": "$iRODSUserName",
+    "irods_zone_name": "$iRODSZoneName",
+    "irods_authentication_scheme": "$iRODSAuthenticationScheme",
+    "irods_encryption_algorithm": "$iRODSEncryptionAlgorithm",
+    "irods_encryption_key_size": $iRODSEncryptionKeySize,
+    "irods_encryption_num_hash_rounds": $iRODSEncryptionNumHashRounds,
+    "irods_encryption_salt_size": $iRODSEncryptionSaltSize,
+    "irods_client_server_negotiation": "$iRODSClientServerNegotiation"
+}
+EOF
+
+# building docker container
+docker image build -t ubuntu-1604-icommands . 
+
+# start docker container
+docker container run --name iRODS-icommands --detach ubuntu-1604-icommands:latest tail -f /dev/null
+
+echo "Connect to container via 'docker run -it ubuntu-1604-icommands:latest bash'"
