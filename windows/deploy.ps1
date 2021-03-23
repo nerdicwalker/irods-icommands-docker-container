d8b0bce9da34d94dbf9c1893e49fa1bc4383556f
nerdicwalker <nerdicwalker@gmail.com>
2021-03-23 15:28:32 +0100

    windows deployment

--- windows/deploy.ps1
+++ windows/deploy.ps1
@@ -1,10 +1,27 @@
+Set-ExecutionPolicy Unrestricted -force
+
 # Input iRODS username
 $iRODSUsername = Read-Host -Prompt 'Please provide your iRODS username? '
+
+"
+{
+    "irods_host": "uva-yoda.irods.surfsara.nl",
+    "irods_port": 1247,
+    "irods_home": "/yoda/home",
+    "irods_user_name": "$iRODSUsername",
+    "irods_zone_name": "yoda",
+    "irods_authentication_scheme": "pam",
+    "irods_encryption_algorithm": "AES-256-CBC",
+    "irods_encryption_key_size": 32,
+    "irods_encryption_num_hash_rounds": 16,
+    "irods_encryption_salt_size": 8,
+    "irods_client_server_negotiation": "request_server_negotiation"
+}
+" | out-file irods_environment.json
 
 # building docker container
 Write-Host "Building container.."
 docker image build -t ubuntu-1804-icommands . 
-rm -rf irods_environment.json
 
 # start docker container
 Write-Host "Starting container.."
@@ -12,7 +29,7 @@ docker container run --name iRODS-icommands --detach ubuntu-1804-icommands:lates
 
 # connecting docker container
 Write-Host "Connecting to the container.."
-docker run -it ubuntu-1804-icommands:latest bash
+docker exec -it iRODS-icommands bash
 
 # stopping docker container
 Write-Host "Stoping the container.."
@@ -22,4 +39,4 @@ docker container stop iRODS-icommands
 Write-Host "Removing container.."
 docker container rm --force iRODS-icommands
 Write-Host "Removing image.."
-docker image rm --force ubuntu-1804-icommands
+docker image rm --force ubuntu-1804-icommands 
